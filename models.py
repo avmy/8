0cd4fb06a60af0187793e62be206ae77ea58465c
avmy <138010448+avmy@users.noreply.github.com>
2023-08-10 19:56:43 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Advertisement(models.Model):
+    title = models.CharField(verbose_name='Название', max_lenth=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Укажите, если возможен торг')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
+
+    class Meta:
+        db_table = 'advertisements'
