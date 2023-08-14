711a785600d18106d260d78fe7bc7a6f3739f59c
avmy <138010448+avmy@users.noreply.github.com>
2023-08-14 16:10:19 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.http import format_html
+from django.utils import timezone
 
 
 class Advertisement(models.Model):
@@ -9,6 +12,14 @@ class Advertisement(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.create.at.date() == timezone.now().date():
+            created_time = self.create_at.time().strtime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}'
+            )
+        return self.create_at.strtime('%d:%m:%Y')
     def __str__(self):
         return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
 
