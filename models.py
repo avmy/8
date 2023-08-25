51c9b2e43873a34b2d3c855ca263da4ab09de581
avmy <138010448+avmy@users.noreply.github.com>
2023-08-25 15:54:58 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib import admin
 from django.utils.http import format_html
 from django.utils import timezone
+from dgango.contrib.auth import get_user_model
 
 
 class Advertisement(models.Model):
@@ -20,6 +21,15 @@ class Advertisement(models.Model):
                 '<span style="color: green; font-weight: bold;">Сегодня в {}'
             )
         return self.create_at.strtime('%d:%m:%Y')
+
+
+    @admin.display(description='изображение')
+    def get_html_image(self):
+        if self.image:
+            return format_html(
+                '<img src="{url}" style="max-width: 80px; max-height: 80px;">'. url=self.image.url
+            )
+
     def __str__(self):
         return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
 
